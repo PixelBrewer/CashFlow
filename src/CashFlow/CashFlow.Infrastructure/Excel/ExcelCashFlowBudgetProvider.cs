7954a2f2f72b547f9060f7c311e6ec6fb2bfdfae
PixelBrewer <pixelbrewer@protonmail.com>
2026-08-13 16:07:48 -0700

    Minimal implementation for importer

--- src/CashFlow/CashFlow.Infrastructure/Excel/ExcelCashFlowBudgetProvider.cs
+++ src/CashFlow/CashFlow.Infrastructure/Excel/ExcelCashFlowBudgetProvider.cs
@@ -1,12 +1,34 @@
 namespace CashFlow.Infrastructure.Excel;
 
+using CashFlow.Core.Enums;
 using CashFlow.Core.Interfaces;
 using CashFlow.Core.Models;
+using ClosedXML.Excel;
 
 public class ExcelCashFlowBudgetProvider(string filePath) : ICashFlowBudgetProvider
 {
     public CashFlowBudgetDefinition GetBudget()
     {
-        throw new NotImplementedException();
+        using var workbook = new XLWorkbook(filePath);
+
+        var worksheet = workbook.Worksheet("Budget");
+
+        var recurringTransactions = new List<RecurringTransaction>();
+
+        var name = worksheet.Cell("B2").GetString();
+        var amount = worksheet.Cell("C2").GetValue<decimal>();
+
+        recurringTransactions.Add(
+            new RecurringTransaction
+            {
+                Id = Guid.NewGuid(),
+                Description = name,
+                Amount = amount,
+                Type = TransactionType.Expense,
+                Frequency = RecurrenceFrequency.Monthly,
+            }
+        );
+
+        return new CashFlowBudgetDefinition { RecurringTransactions = recurringTransactions };
     }
 }
