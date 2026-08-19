ea49b083bd3dbc2bec98a9123151ec0dd056ccb8
PixelBrewer <pixelbrewer@protonmail.com>
2026-08-19 01:05:09 -0700

    Cleaning up renaming artifacts

--- src/CashFlow/CashFlow.Api/Models/Requests/ProjectionRequest.cs
+++ src/CashFlow/CashFlow.Api/Models/Requests/ProjectionRequest.cs
@@ -2,7 +2,7 @@ namespace CashFlow.Api.Models.Requests;
 
 using CashFlow.Core.Models;
 
-public class CashFlowProjectionRequest
+public class ProjectionRequest
 {
     public decimal OpeningBalance { get; set; }
     public IReadOnlyList<ScheduledTransaction> Transactions { get; set; } = [];
